0fd831199dd492e66007fcbe7d87e4a5dfe6ba6a
Andre Kristian <andre.m.kristian@gmail.com>
2024-02-07 18:04:19 -0800

    End of section 7 (Error handling)

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -22,7 +22,6 @@ export class NavComponent implements OnInit {
   login() {
     this.accountServices.login(this.model).subscribe({
       next: () => this.router.navigateByUrl('/members'),
-      error: error => this.toastr.error(error.error)
     })
   }
 
