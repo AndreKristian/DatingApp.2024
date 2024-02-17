cb7c96b9758749f917785c6bbe290fe3cab33841
Andre Kristian <andre.m.kristian@gmail.com>
2024-02-16 19:33:00 -0800

    Updating Resources (Section 10)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -38,5 +39,20 @@ public class UsersController : BaseApiController
         var user = await _userRepository.GetUserByUsernameAsync(username);
 
         return _mapper.Map<MemberDto>(user);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null) return NotFound();
+
+        _mapper.Map(memberUpdateDto, user);
+
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update user");
     }
 }
