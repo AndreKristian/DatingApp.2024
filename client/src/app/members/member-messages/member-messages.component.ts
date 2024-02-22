c08b6251ea574e8edf88cfb96666c801b00a026c
Andre Kristian <andre.m.kristian@gmail.com>
2024-02-21 23:05:56 -0800

    Message tab with Route Resolver

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,31 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { TimeagoModule } from 'ngx-timeago';
 import { Message } from 'src/app/_models/message';
-import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, TimeagoModule]
 })
 export class MemberMessagesComponent implements OnInit {
   @Input() username?: string;
-  messages: Message[] = [];
+  @Input() messages: Message[] = [];
 
-  constructor(private messageService: MessageService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.loadMessages();
   }
 
-  loadMessages() {
-    if (this.username) {
-      this.messageService.getMessageThread(this.username).subscribe({
-        next: messages => this.messages = messages
-      })
-    }
-  }
 
 }
